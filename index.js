ce4efbf9dd6a3b11cf736cbb11d979dadcaad79f
JamesWilson <wesstation2@gmail.com>
2017-10-24 14:09:24 +0800

    查看路由狀況

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const app = require('fastify')()
 
-app.get('/',function(req,res){
-  res.send({hello: 'world'})
+const schema = {
+  response: {
+    200: {
+      type:"object",
+      properties: {
+        hello: {
+          type: "string"
+        }
+      }
+    }
+  }
+}
+
+app.get('/',schema,function(req,res){
+  res.send({ hello: 'world' })
 })
 
+app.ready(() => {
+  for (var route of app){
+    console.log(route)
+  }
+})
 
 app.listen(1637)
